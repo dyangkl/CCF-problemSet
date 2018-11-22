6b85337bc48094b5919c402b1d3746756e772569
CreatorKarl <995199838@qq.com>
2018-11-22 23:20:02 +0800

    .

--- temp.py
+++ temp.py
@@ -1,10 +1,1 @@
-# 最小差值
-n = eval(input())
-li = list(map(int,input().split()))
-# 列表排序
-li.sort()
-
-# 后一项减去前一项的差值
-difference = list(map(lambda x , y : y - x , li[: -1] , li[ 1:]))
-
-print(min(difference))
+print(1<=1<=1)
